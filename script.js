c231fdbfc3449c837a0bba5a7fb76ceb63f2f4f0
Gerardo Sanchez <oracle942@gmail.com>
2021-08-12 23:31:47 -0700

    Created variables for prompts and confirm messages. Added event listener. Created empty array to hold password.

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 // Assignment Code
-var lengthPrompt = prompt("Please choose a password length between 8 and 128 characters")
-var specialPrompt = prompt("Should the password contain special characters?")
-var lowercasePrompt = prompt("Should the password contain lowercase characters?")
-var uppercasePrompt = prompt("Should the password contain uppercase characters?")
-var numbersPrompt = prompt("Should the password contain numbers?")
-
-
 var generateBtn = document.querySelector("#generate");
+var passwordArray = [];
+
+// Add event listener to generate button
+if(generateBtn){
+    generateBtn.addEventListener("click", () => {
+        var lengthInput = +length;
+        var symbolsInput =  symbols;
+        var upperInput = uppercase;
+        var lowerInput = lowercase;
+        var numbersInput = numbers;
+
+    console.log(lengthInput, symbolsInput, upperInput, lowerInput, numbersInput);
+  })
+};
+
+
+
+const randomFunc = { 
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -16,9 +32,16 @@ function writePassword() {
   passwordText.value = password;
 
 }
+var length = prompt ("Please choose a password length between 8 and 128 characters", "Enter number between 8 and 128");
+var symbols =  confirm ("Should the password contain special characters?");
+var lowercase =  confirm ("Should the password contain lowercase characters?");
+var uppercase =  confirm ("Should the password contain uppercase characters?");
+var numbers =  confirm ("Should the password contain numbers?");
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Test for correct length user input
+// if (length < 8 || length > 128 || length !== int) {
+//   alert ("Please enter a number between 8 and 128");
+
 
 
 function getRandomLower() {
@@ -36,3 +59,14 @@ function getRandomNumber() {
 function getRandomSymbol() {
 	const symbols = '!@#$%^&*(){}[]=<>/,.+-_|:;?~`'
 	return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+
+
+// function confirmInput() {
+//   var trueFalse;
+//   if (confirm()) {
+//     bool = true;
+//   } else {
+//     bool = false;
+//   }
