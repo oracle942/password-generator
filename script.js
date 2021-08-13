19cf56a2bef701e22b165563d5c68e99a75a29b6
Gerardo Sanchez <oracle942@gmail.com>
2021-08-12 19:16:41 -0700

    Random character generator functions added. Readme includes project instructions.

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 // Assignment Code
+var lengthPrompt = prompt("Please choose a password length between 8 and 128 characters")
+var specialPrompt = prompt("Should the password contain special characters?")
+var lowercasePrompt = prompt("Should the password contain lowercase characters?")
+var uppercasePrompt = prompt("Should the password contain uppercase characters?")
+var numbersPrompt = prompt("Should the password contain numbers?")
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -12,3 +19,20 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function getRandomLower() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+	return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+	const symbols = '!@#$%^&*(){}[]=<>/,.+-_|:;?~`'
+	return symbols[Math.floor(Math.random() * symbols.length)];
