ed48e8cfb6c1f5e01d00ce8f2fcd137fd54a858b
Gerardo Sanchez <oracle942@gmail.com>
2021-08-14 12:39:13 -0700

    Tentative final version of application. JS comments included.

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-var password = '';
 
-
+// Random characters and numbers generators.
   function getRandomSymbol() {
 	const symbols = '!@#$%^&*(){}[]=<>/,.+-_|:;?~`'
 	  return symbols[Math.floor(Math.random() * symbols.length)]};
@@ -18,88 +16,60 @@ var password = '';
 
   function getRandomNumber() {
 	  return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
+}            
   
-                      
-  
+// Begin main algorithm.
 if(generateBtn){
     
     generateBtn.addEventListener("click", () => {
     var length = prompt ("Please choose a password length between 8 and 128 characters", "Enter number between 8 and 128");
     length = +length;
-    
-    if (!length){
-      return;}
-  
+   
+    // Character length and type input condition check.
     if (length < 8 || length > 128 || !Number.isInteger(length)) {
-      window.alert ("Please enter a number between 8 and 128");}
+      window.alert ("Please enter a number between 8 and 128");
+    return;}
+    
 
     else{
+    // Assign inputs to variables.
     var symbols =  confirm ("Should the password contain special characters?");
     var lowercase =  confirm ("Should the password contain lowercase characters?");
     var uppercase =  confirm ("Should the password contain uppercase characters?");
     var numbers =  confirm ("Should the password contain numbers?");
     var inputCount = symbols + lowercase + uppercase + numbers;
-    var charGenArray = [];
-    
 
+    // Create empty arrays.
+    var charGenArray = [];
+    var passwordArray = [];
+
+
+    // Minimally required user selction verification.
     if(inputCount === 0) {
       window.alert("Please affirm at least one selection.")
       return;}
 
-    if(symbols === true){
-      charGenArray.push(getRandomSymbol());}
-    if(lowercase === true){
-      charGenArray.push(getRandomLower());}
-    if(uppercase === true){
-      charGenArray.push(getRandomUpper());}
-    if(numbers === true){
-      charGenArray.push(getRandomNumber());}
+    
+      // Password generator. Password pushed into charGenArray.
+      for(i = 0; i < length; i++){
+      if(symbols === true){
+        charGenArray.push(getRandomSymbol());}
+      if(lowercase === true){
+        charGenArray.push(getRandomLower());}
+      if(uppercase === true){
+        charGenArray.push(getRandomUpper());}
+      if(numbers === true){
+        charGenArray.push(getRandomNumber());}
+        
+      passwordArray.splice(0, 0, charGenArray[Math.floor(Math.random() * charGenArray.length)])
+      
+    }
 
-      
-      
-      
-      
-      
-      console.log(charGenArray);
-    
-      
+    // Convert password array into string without commas.
+    var password = passwordArray.join("");
     
     
-    
-    
-    
-      
-   
-      
-      
-    //   // create a loop
-    //   for(let i=0; i<length; i+=typesCount) {
-    //     typesArr.forEach(type => {
-    //       const funcName = Object.keys(type)[0];
-    //       generatedPassword += randomFunc[funcName]();
-    //     });
-    //   }
-      
-      
-    }     
-    
-    
+    // Display password in viewport.
+    document.getElementById("password").innerHTML = password;    
 
-
-  })
-}
-
-
-// function writePassword() {
-//  getRandomAF1 = password;
-//  return
-// }
-
-// // function confirmInput() {
-// //   var trueFalse;
-// //   if (confirm()) {
-// //     bool = true;
-// //   } else {
-// //     bool = false;
-// //   }
+}})}  
